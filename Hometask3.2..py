7fe5967f322acb4723f4d0052b2889664cd4d15f
Oksana Sviechikhina <Ksushenka2994@ukr.net>
2025-02-06 13:57:47 +0200

    ДЗ 3.2. Перемістити елемент у списку

--- Hometask3.2..py
+++ Hometask3.2..py
@@ -1,15 +1,5 @@
-#1
 lst = [12, 3, 4, 10, 8]
 if len(lst) > 1:
     last_element = lst.pop()
     lst.insert(0, last_element)
 print(lst)
-
-# або
-#2
-lst_1 = [12, 3, 4, 10, 8]
-if len(lst_1) > 1:
-    last_element_1 = lst_1[-1]
-    lst_1 = [last_element_1] + lst_1[:-1]
-
-print(lst_1)
