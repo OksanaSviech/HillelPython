12249e3140b2c82125a77aad9d751a4e171534f3
Oksana Sviechikhina <Ksushenka2994@ukr.net>
2025-02-07 17:22:41 +0200

    ДЗ 4.2. Знайти суму елементів із парними індексами

--- Hometask4.2.py
+++ Hometask4.2.py
@@ -1,5 +1,20 @@
 lst = [0, 1, 7, 2, 4, 8]
-new_list = lst[::2]
-last_value = lst.pop(-1)
-result = sum(new_list) * last_value
-print(result)
+
+if len(lst)==0:
+    print('0')
+else:
+    result = sum(lst[::2]) * lst[-1]
+    print(result)
+
+
+
+
+# lst = [0, 1, 7, 2, 4, 8]
+# sum_even_indices = 0
+# if len(lst) == 0:
+#     print('0')
+# else:
+#     for i in range(0, len(lst), 2):
+#         sum_even_indices += lst[i]
+#     result = sum_even_indices * lst[-1]
+#     print(result)
